eeb6c0ea72cfb7a5eeb1573c04696658b01b0282
brianlu <eebrian123tw93@gmail.com>
2018-12-28 04:14:05 +0800

    cc

--- script.sh
+++ script.sh
@@ -1,4 +1,4 @@
-mvn clean
-mvn compile
-mvn assembly:assembly
-java -jar ./target/Test-1.0-SNAPSHOT-jar-with-dependencies.jar
+sudo mvn clean
+sudo mvn compile
+sudo mvn assembly:assembly
+sudo java -jar ./target/Test-1.0-SNAPSHOT-jar-with-dependencies.jar
